607c7604f8f725e6b9212eded95d627e1a4bf451
Rase <rasengithub@protonmail.com>
2022-04-30 10:13:22 +0300

    Added some commenting.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,34 +8,50 @@ return require('packer').startup(function(use)
   -- My plugins here
   use 'wbthomason/packer.nvim'
 
+-- Easy language server installer etc.
 use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'} -- Collection of configurations for the built-in LSP client
+
+-- Telescope fuzzy finder
 use {
   'nvim-telescope/telescope.nvim',
   requires = { {'nvim-lua/plenary.nvim'} }
 }
+use 'nvim-telescope/telescope-fzy-native.nvim'
+
+-- cmp
+use 'hrsh7th/nvim-cmp'
 use 'hrsh7th/cmp-nvim-lsp'
 use 'hrsh7th/cmp-buffer'
 use 'hrsh7th/cmp-path'
 use 'hrsh7th/cmp-cmdline'
 use 'hrsh7th/cmp-nvim-lua'
-use 'vim-airline/vim-airline'
-use 'tpope/vim-fugitive'
-use 'morhetz/gruvbox'
-use 'tpope/vim-surround'
-use 'rafamadriz/friendly-snippets'
-use 'hrsh7th/nvim-cmp'
 use 'L3MON4D3/LuaSnip'
+use 'rafamadriz/friendly-snippets'
 use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-use 'nvim-telescope/telescope-fzy-native.nvim'
-use 'ThePrimeagen/harpoon'
-use 'sbdchd/neoformat'
-use 'folke/todo-comments.nvim'
-use 'github/copilot.vim'
-use 'mfussenegger/nvim-dap'
-use {'mg979/vim-visual-multi', branch='master'}
-use 'rcarriga/nvim-dap-ui'
-use 'onsails/lspkind.nvim'
+
+-- Gods plugins
+use 'tpope/vim-fugitive'
 use 'tpope/vim-commentary'
+use 'tpope/vim-surround'
+
+-- Some twerking shit for todo comments.
+use 'folke/todo-comments.nvim'
+
+-- Multi cursor thingy.
+use {'mg979/vim-visual-multi', branch='master'}
+
+-- Debugging thing that probably will never be used.
+use 'mfussenegger/nvim-dap'
+use 'rcarriga/nvim-dap-ui'
+
+-- Formatting plugin that is shit.
+use 'sbdchd/neoformat'
+
+use 'vim-airline/vim-airline'
+use 'morhetz/gruvbox'
+use 'ThePrimeagen/harpoon'
+use 'github/copilot.vim'
+use 'onsails/lspkind.nvim'
 use 'turbio/bracey.vim'
 use 'preservim/nerdtree'
 use 'ryanoasis/vim-devicons'
