07a6d3f99e50e5c335de5a0cab6df5c3856f57e6
Rase <rasengithub@protonmail.com>
2022-04-30 12:00:33 +0300

    Kk command highlighting works too now.

--- init.lua
+++ init.lua
@@ -129,20 +129,20 @@ cmp.setup({
 
 
     -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline('/', {
-    sources = {
-      { name = 'buffer' }
-    }
-    })
+--    cmp.setup.cmdline('/', {
+--    sources = {
+--      { name = 'buffer' }
+--    }
+--    })
 
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline(':', {
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline' }
-      })
-      })
+--    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+--    cmp.setup.cmdline(':', {
+--      sources = cmp.config.sources({
+--    --    { name = 'path' }
+--      }, {
+--    --    { name = 'cmdline' }
+--      })
+--      })
 
 -- LSP --
 local lsp_installer = require("nvim-lsp-installer")
