454ae7fa200ed303d783514d152f5b0d4d9e9757
Rase <>
2022-05-06 19:50:43 +0300

    Added another debugger instead.

---
+++ lua/config/dap.lua
@@ -0,0 +1,38 @@
+local dap = require('dap')
+vim.fn.sign_define('DapBreakpoint', {text='🟥', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointRejected', {text='🟦', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text='⭐️', texthl='', linehl='', numhl=''})
+
+-- 💀 Adjust the path to your executable
+--
+local path = vim.env.HOME .. '/.config/nvim/adapters/extension/debugAdapters/OpenDebugAD7'
+dap.adapters.cppdbg = {
+  id = 'cppdbg',
+  type = 'executable',
+  command = path,
+}
+
+dap.configurations.rust = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = path,
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
