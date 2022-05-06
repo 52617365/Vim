d8cc6ac7f01d0171cbc1558d78e8140775b70bd3
Rase <>
2022-05-06 17:11:09 +0300

    Added Trouble for diagnostics.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -52,3 +52,22 @@ api.nvim_set_keymap('n', '<Leader>s', ':BrowserSearch ', non_silent_opts)
 --api.nvim_set_keymap('v', '<Leader>s', '<Plug>SearchVisual',  opts)
 api.nvim_set_keymap('v', '<Leader>s', ':BrowserSearch<CR>',  opts)
 
+-- Lua
+vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
+  {silent = true, noremap = true}
+)
