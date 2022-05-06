ee887feea37b92c2bbe2c2d7dcbd41414523bc0b
Rase <rasengithub@protonmail.com>
2022-05-06 17:04:01 +0300

    This commit consists of a snippet that makes code diagnostics appear with lua vim.diagnostic.open_float() instead of showing inline.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -44,6 +44,22 @@ for _, server in ipairs(installable_servers) do
   require('lspconfig')[server].setup { capabilities = capabilities, on_attach = on_attach }
 end
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- This will disable virtual text, like doing:
+    virtual_text = false,
+
+    -- This is similar to:
+    -- let g:diagnostic_show_sign = 1
+    -- To configure sign display,
+    --  see: ":help vim.lsp.diagnostic.set_signs()"
+    signs = true,
+
+    -- This is similar to:
+    -- "let g:diagnostic_insert_delay = 1"
+    update_in_insert = false,
+  }
+)
 
 -- lspconfig.rust_analyzer.setup{
 --   on_attach = on_attach,
