48205f0e88c7802602e9a01504f4c81566dc838e
Rase <>
2022-05-07 17:57:00 +0300

    Added toggle to open and close nerdtree on tt

--- lua/options.lua
+++ lua/options.lua
@@ -19,13 +19,17 @@ augroup YankHighlight
   autocmd!
   autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
 augroup END
+
+augroup Closetree
+  autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
+augroup END
 ]])
 -- augroup nerdtree
 --   autocmd VimEnter * NERDTree | wincmd p
 --   autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
 -- augroup end
 --
-g.deus_termcolors=256
+g.deus_termcolors = 256
 api.nvim_command('colorscheme deus')
 opt.path = "$PWD/**"
 opt.undodir = os.getenv('HOME') .. "/temp/"
@@ -38,26 +42,26 @@ opt.iskeyword:append("-")
 opt.shortmess:append("c")
 opt.hidden = true
 opt.number = true
-opt.splitbelow = true                       -- force all horizontal splits to go below current window
-opt.splitright = true                       -- force all vertical splits to go to the right of current window
+opt.splitbelow = true -- force all horizontal splits to go below current window
+opt.splitright = true -- force all vertical splits to go to the right of current window
 opt.relativenumber = true
 opt.ignorecase = true
-opt.pumheight = 10  -- pop up menu height
-opt.showtabline = 2                         -- always show tabs
-opt.smartcase = true                        -- smart case
+opt.pumheight = 10 -- pop up menu height
+opt.showtabline = 2 -- always show tabs
+opt.smartcase = true -- smart case
 opt.shiftwidth = 2
 opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
 opt.updatetime = 300
 opt.autowriteall = true
 opt.smartindent = true
-opt.splitbelow = true                       -- force all horizontal splits to go below current window
-opt.splitright = true                       -- force all vertical splits to go to the right of current window
-opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-opt.wrap = false   -- display lines as one long line
-opt.cursorline = true                       -- highlight the current line
+opt.splitbelow = true -- force all horizontal splits to go below current window
+opt.splitright = true -- force all vertical splits to go to the right of current window
+opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
+opt.wrap = false -- display lines as one long line
+opt.cursorline = true -- highlight the current line
 opt.guifont = "monospace 17"
 opt.termguicolors = true
-opt.scrolloff = 8  -- is one of my fav
+opt.scrolloff = 8 -- is one of my fav
 opt.sidescrolloff = 8
 opt.undofile = true
 opt.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
